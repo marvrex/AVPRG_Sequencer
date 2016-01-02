a032e2643da1433390b46898f35839d535a324c5
Marv <proxynoxii@gmail.com>
2016-01-02 14:58:48 +0100

    added shapedetection. Detect shapes will be written to a new image

--- avprg/avprg/main.cpp
+++ avprg/avprg/main.cpp
@@ -5,9 +5,17 @@ using namespace cv;
 using namespace std;
 
 void getShapes(Mat src, int thresh);
+void drawShapeContours(Mat src, int thresh);
+void setLabel(cv::Mat& im, const std::string label, std::vector<cv::Point>& contour);
+static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0);
+
 int verbinden(vector<vector<Point> > shapes);
 int thresh = 150;
 int max_thresh = 255;
+
+//debug code
+String imageDirectory = "C:\\dev\\git\\AVPRG_Sequencer\\avprg\\test3.jpg";
+RNG rng(120);
 
 int main(){
 
@@ -21,23 +29,18 @@ int main(){
 
 	if (!isImage){
 		for (;;){
-
-
-
 			//Load frame
 			cap >> frame;
 
-
 			//getShapes(frame);
 
-
-
+			//break
 			if (waitKey(30) >= 0) break;
 		}
 	}
 	else{
-
-		Mat src = imread("C:\\Users\\Timmi\\Documents\\Visual Studio 2013\\Projects\\avprg\\test.jpg", 1);   // Read the file
+		//debug section
+		Mat src = imread(imageDirectory, 1);   // Read the file
 
 		if (!src.data)                              // Check for invalid input
 		{
@@ -45,36 +48,162 @@ int main(){
 			return -1;
 		}
 
-
+		createTrackbar("Canny thresh:", "src", &thresh, max_thresh);
 		imshow("src", src);
-		createTrackbar(" Canny thresh:", "src", &thresh, max_thresh);
 
 		
-		//while (true){
-			getShapes(src, thresh);
-		//	if (waitKey(50) == 27){
-		//		break;
-		//	}
-		//}
-			waitKey(0);
-
-
-
-		
-
-		
+		while (true){
+			//getShapes(src, thresh);
+			drawShapeContours(src, thresh);
+			if (waitKey(50) == 27){
+				break;
+			}
+		}
+			waitKey(0);	
 	}
 
 	return 0;
 }
 
+void drawShapeContours(Mat src, int thresh) {
+	Mat gray;
 
+	//convert to gray, blur and apply a threshold
+	cvtColor(src, gray, CV_BGR2GRAY);
+	GaussianBlur(gray, gray, Size(9, 9), 2, 2);
+	threshold(gray, gray, thresh, 255, CV_THRESH_BINARY);
+
+	//detect edges
+	Mat edges = gray.clone();
+	Canny(gray, edges, 100, 200, 3, true);
+
+	//opening um nur eine linie zu bekommen
+	dilate(edges, edges, Mat(), Point(-1, -1), 1); 
+	erode(edges, edges, Mat(), Point(-1, -1), 1);
+
+	//contours and their hierarchy
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+
+	//find contours and draw them to a third image (contoursImg)
+	findContours(gray, contours, hierarchy, RETR_LIST, CV_CHAIN_APPROX_NONE);
+	Mat contoursImg = Mat::zeros(src.size(), CV_8UC3);
+
+	cout << contours.size();
+	for (int i = 0; i < contours.size(); i++) {
+		Scalar color = Scalar(125, 125, 125);
+		drawContours(contoursImg, contours, i, Scalar(125, 125, 125), 1, 8, hierarchy, 0, Point());
+	}
+
+	//show contoursImg
+	imshow("contours", contoursImg);
+
+	//approximate contours and write them to result
+	Mat result = src.clone();
+
+	//approx will contain the vertices of our objects
+	vector<cv::Point> approx;
+	int triangleCount = 0;
+	int rectCount = 0;
+	int circleCount = 0;
+
+	for (int i = 0; i < contours.size(); i++) {
+		cv::approxPolyDP(cv::Mat(contours[i]),approx, cv::arcLength(cv::Mat(contours[i]), true) * 0.02,true);
+
+		//skip small and non-convex shapes
+		if (std::fabs(cv::contourArea(contours[i])) < 100 || !cv::isContourConvex(approx) )
+			continue;
+
+		//Triangle
+		if (approx.size() == 3) {
+			setLabel(result, "TRI", contours[i]);
+			triangleCount++;
+		}
+		//rectangle and hexagon
+		else if (approx.size() >= 4 && approx.size() <= 6) {
+			//number ov vertices
+			int vertices = approx.size();
+
+			//get degree of all corners
+			std::vector<double> cos;
+			for (int j = 2; j < vertices+1; j++) {
+				cos.push_back(angle(approx[j%vertices], approx[j-2], approx[j-1]));
+			}
+			
+			//sort ascending the angles
+			std::sort(cos.begin(), cos.end());
+
+			//lowest and highest angle?
+			double minCos = cos.front();
+			double maxCos = cos.back();
+
+			if (vertices == 4 && minCos >= -0.1 && maxCos <= 0.3) {
+				cv::Rect rectangle = cv::boundingRect(contours[i]);
+				double ratio = std::abs(1 - (double) rectangle.width / rectangle.height);
+
+				//write our recctangle
+				setLabel(result,"RECT", contours[i]);
+				rectCount++;
+			} else if (vertices == 5 && minCos >= -0.34 && maxCos <= -0.27)
+				setLabel(result, "PENTA", contours[i]);
+			else if (vertices == 6 && minCos >= -0.55 && maxCos <= -0.45)
+				setLabel(result, "HEXA", contours[i]);
+		}
+		else {
+			//if the shape isnt a triangle, rectangle or hexagon, it should be a circle
+			double area = cv::contourArea(contours[i]);
+			cv::Rect rect = cv:: boundingRect(contours[i]);
+			int radius = rect.width / 2;
+
+			
+		 if (std::abs(1 - ((double)rect.width / rect.height)) <= 0.2 &&
+			 std::abs(1 - (area / (CV_PI * std::pow((float) radius, (float)2)))) <= 0.2) {
+				setLabel(result, "CIRCLE", contours[i]);
+				circleCount++;
+			}
+		}
+
+	//end of loop
+	}
+	imshow("result", result);
+}
+
+/**
+ * Helper function to find a cosine of angle between vectors
+ * from pt0->pt1 and pt0->pt2
+ */
+static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0)
+{
+	double dx1 = pt1.x - pt0.x;
+	double dy1 = pt1.y - pt0.y;
+	double dx2 = pt2.x - pt0.x;
+	double dy2 = pt2.y - pt0.y;
+	return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
+}
+
+/**
+ * Helper function to display text in the center of a contour
+ */
+void setLabel(cv::Mat& im, const std::string label, std::vector<cv::Point>& contour)
+{
+	int fontface = cv::FONT_HERSHEY_SIMPLEX;
+	double scale = 0.4;
+	int thickness = 1;
+	int baseline = 0;
+
+	cv::Size text = cv::getTextSize(label, fontface, scale, thickness, &baseline);
+	cv::Rect r = cv::boundingRect(contour);
+
+	cv::Point pt(r.x + ((r.width - text.width) / 2), r.y + ((r.height + text.height) / 2));
+	cv::rectangle(im, pt + cv::Point(0, baseline), pt + cv::Point(text.width, -text.height), CV_RGB(255,255,255), CV_FILLED);
+	cv::putText(im, label, pt, fontface, scale, CV_RGB(0,0,0), thickness, 8);
+}
 
 void getShapes(Mat src, int thresh){
 
 	namedWindow("result", WINDOW_AUTOSIZE);
 	namedWindow("contours_drawing", WINDOW_AUTOSIZE);
-
+	namedWindow("contours", WINDOW_AUTOSIZE);
 
 
 	Mat gray, src_copy;
@@ -88,7 +217,6 @@ void getShapes(Mat src, int thresh){
 	threshold(gray, gray, 245, 255, 0);
 	GaussianBlur(gray, gray, Size(9, 9), 2, 2);
 
-
 	Mat canny_output;
 	vector<vector<Point> > contours;
 	vector<vector<Point> > shapes;
@@ -97,25 +225,23 @@ void getShapes(Mat src, int thresh){
 
 	// Detect edges using canny
 	Canny(gray, canny_output, thresh, thresh*2, 3);
-
+	
 	//dilate um nur eine linie zu bekommen
 	dilate(canny_output, canny_output, Mat(), Point(-1, -1), 1); 
 	erode(canny_output, canny_output, Mat(), Point(-1, -1), 1);
 
-	
-
-	// Find contours
+	 //find contours
 	findContours(canny_output, contours, hierarchy, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
 	for (int i = 0; i < contours.size(); i++){
 		drawContours(contours_drawing, contours, i, Scalar(125, 125, 125), 1, 8, hierarchy, 0, Point());
 	}
 	imshow("contours_drawing", contours_drawing);
-
+	imshow("contours", contours);
 
 	shapes.resize(contours.size());
-	int anzahlQuadrate = 0;
-	int anzahlDreiecke = 0;
 
+	int anzahlquadrate = 0;
+	int anzahldreiecke = 0;
 
 	//gehe alle konturen durch
 	for (size_t i = 0; i < contours.size(); i++){
@@ -125,7 +251,7 @@ void getShapes(Mat src, int thresh){
 
 
 		//zeichnen der figur
-		cout << "Figur gefunden mit " + to_string(shapes[i].size()) + " Ecken.";
+		//cout << "figur gefunden mit " + to_string(shapes[i].size()) + " ecken.";
 		cout << "\n";
 		for (int j = 0; j < shapes[i].size(); j++){
 			if (j == (shapes[i].size() - 1)){
@@ -137,9 +263,5 @@ void getShapes(Mat src, int thresh){
 		}
 	}
 
-
-	
 	imshow("result", result);
-	
-
 }
